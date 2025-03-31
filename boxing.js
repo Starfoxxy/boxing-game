1e7161a83596c8bd6753a9d0f0a3abf0753283e4
Starfoxxy <athenajacob97@gmail.com>
2025-03-31 06:29:30 -0400

    Added function
    Added function to generate random damage

--- boxing.js
+++ boxing.js
@@ -10,3 +10,8 @@ let player2 = {
 };
 
 let currentPlayer = player1;
+
+// Function to generate random damage
+function generateDamage() {
+    return Math.floor(Math.random() * 10) + 1; // Damage ranges from 1 to 10
+    }
