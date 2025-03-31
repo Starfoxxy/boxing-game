038b2cebc8c94e125d1be6083464859c28064a45
Starfoxxy <athenajacob97@gmail.com>
2025-03-31 06:46:43 -0400

    Added game over function
    Added function to check if the game is over through knockout score and dot notation conditional

--- boxing.js
+++ boxing.js
@@ -19,16 +19,26 @@ function generateDamage() {
     // Function to apply damage to the opponent
     function applyDamage(opponent, damage) {
         if (damage < 6) {
-        console.log(`${currentPlayer.name} lands a basic hit on ${opponent.name}!`);
+            console.log(`${currentPlayer.name} lands a basic hit on ${opponent.name}!`);
         } else if (damage >=  6 && damage <= 9) {
-        console.log(`${currentPlayer.name} lands a critical hit on ${opponent.name}!`);
+            console.log(`${currentPlayer.name} lands a critical hit on ${opponent.name}!`);
         } else {
-        console.log(`${currentPlayer.name} lands a KNOCKOUT on ${opponent.name}!🥊`);
+            console.log(`${currentPlayer.name} lands a KNOCKOUT on ${opponent.name}!🥊`);
         }
         opponent.health -= damage;
         if (opponent.health < 0 || damage === 10) {
-        // Ensure health doesn't go below 0 and automatic KO
-        opponent.health = 0;
+            // Ensure health doesn't go below 0 and automatic KO
+            opponent.health = 0;
         }
-        console.log(`${opponent.name}'s health is now ${opponent.health}.`);
+            console.log(`${opponent.name}'s health is now ${opponent.health}.`);
     }
+      // Function to check if the game is over
+      function checkKnockout(opponent) {
+        if (opponent.health <= 0) {
+            console.log(`${opponent.name} has been knocked out!`);
+            console.log(`${currentPlayer.name} is the WINNER! 🏆`);
+            return true; // Game over
+        }
+        return false;
+      }
+        
